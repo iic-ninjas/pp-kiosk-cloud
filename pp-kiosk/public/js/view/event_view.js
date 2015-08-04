fa2e9121977f61dd36bddb7893dd1920acb8a070
Gilad Goldberg <giladgo@gmail.com>
2015-08-04 20:50:43 +0300

    Adding edit link

--- pp-kiosk/public/js/view/event_view.js
+++ pp-kiosk/public/js/view/event_view.js
@@ -2,10 +2,15 @@ var EventView = Marionette.ItemView.extend({
 	className: 'event',
 	template: '#event-template',
 	events: {
-		'click .payment-link': '_paymentLinkClick'
+		'click .payment-link': '_paymentLinkClick',
+		'click .edit-link': '_editLinkClick'
 	},
 
 	_paymentLinkClick: function() {
 		window.app.navigate('payments', {trigger: true});
+	},
+
+	_editLinkClick: function() {
+		window.app.navigate('events/' + this.model.id + '/edit', {trigger: true});
 	}
 });
