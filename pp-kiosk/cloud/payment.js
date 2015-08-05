42308811c7285be1929fd17db65bd97b887f4a59
Gilad Goldberg <giladgo@gmail.com>
2015-08-05 03:15:43 +0300

    Flatten out video

--- pp-kiosk/cloud/payment.js
+++ pp-kiosk/cloud/payment.js
@@ -4,9 +4,10 @@ var Greeting = Parse.Object.extend("Greeting");
 var Payment = Parse.Object.extend("Payment", {
 
 }, {
-	create: function(event, paymentData) {
+	create: function(event, paymentData, greetingVideo) {
 		var payment = new Payment(_.omit(paymentData, 'greeting'));
-		var greeting = new Greeting(paymentData.greeting);
+		var greeting = new Greeting();
+		greeting.set('video', greetingVideo); 
 
 		return greeting.save().then(function(){
 			payment.set('greeting', greeting);
