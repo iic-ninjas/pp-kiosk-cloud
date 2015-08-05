cbdeb70875825c14f25008df51f61071d2860b12
Oded Argon <oargon@paypal.com>
2015-08-05 05:40:41 +0300

    More payments fixes

--- pp-kiosk/public/js/app.js
+++ pp-kiosk/public/js/app.js
@@ -11,6 +11,21 @@ var AppLayoutView = Marionette.LayoutView.extend({
 $( document ).ready(function() {
 	Parse.initialize("2tPV0437veVV9ef6egrkdzGxu6qSpthN8fYfY8AK", "Hm9CwIxz0XESJejp4HLFObieMQ9yvVan9nbqFN90");
 
+	$(".logout-link").click(function() {
+  		Parse.User.logOut();
+		var currentUser = Parse.User.current();  // this will now be null
+		// $("#logout-wrapper").hide();
+		window.location.href = "index.html";
+	});
+
+	var currentUser = Parse.User.current();
+	if (currentUser) {
+  		// user is logged in
+  		$("#logout-wrapper").show();
+	} else {
+    	// show the signup or login page
+    }
+
 	window.appLayout = new AppLayoutView();
 	window.appLayout.render();
 
--- pp-kiosk/public/js/index.js
+++ pp-kiosk/public/js/index.js
@@ -1,22 +1,16 @@
 function processLoginForm(e) {
     if (e.preventDefault) e.preventDefault();
 
-    /* do what you want with the form */
-    console.log("loginng in");
-    console.log($("#login-username").val());
-    console.log($("#login-password").val());
-
     Parse.User.logIn($("#login-username").val(), $("#login-password").val(), {
   		success: function(user) {
     		// Do stuff after successful login.
-    		console.log("success");
-    		console.log(user);
     		window.location.href = "app.html";
   		},
   		error: function(user, error) {
     		// The login failed. Check error to see why.
-    		console.log("login failed");
-    		console.log(error);
+            // alert("Error: " + error.code + " " + error.message);
+            $(".form-group").addClass("has-error");
+            alert("Error: " + error.code + " " + error.message);
   		}
 	});
 
@@ -26,6 +20,24 @@ function processLoginForm(e) {
 
 $( document ).ready(function() {
 	Parse.initialize("2tPV0437veVV9ef6egrkdzGxu6qSpthN8fYfY8AK", "Hm9CwIxz0XESJejp4HLFObieMQ9yvVan9nbqFN90");
+
+    $("#create-account").click(function() {
+        var user = new Parse.User();
+        user.set("username", $("#login-username").val());
+        user.set("password", $("#login-password").val());
+        user.set("email", $("#login-username").val());
+
+        user.signUp(null, {
+          success: function(user) {
+            // Hooray! Let them use the app now.
+            window.location.href = "app.html";
+          },
+          error: function(user, error) {
+            // Show the error message somewhere and let the user try again.
+            alert("Error: " + error.code + " " + error.message);
+          }
+        });
+    });
 
 	var form = document.getElementById('login-form');
 	if (form.attachEvent) {
@@ -37,10 +49,8 @@ $( document ).ready(function() {
 	var currentUser = Parse.User.current();
 	if (currentUser) {
     	// do stuff with the user
-    	console.log(currentUser);
     	window.location.href = "app.html";
 	} else {
     	// show the signup or login page
-    	console.log("need to login");
     }
 });
